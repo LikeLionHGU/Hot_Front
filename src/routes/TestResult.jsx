baf88e1f07674d39e06f601a45d6c22a157cad11
Seonghwan Sim <hoanshim01@handong.ac.kr>
2024-05-19 22:53:33 +0900

    style : 테스트 결과페이지 header 추가

---
+++ src/routes/TestResult.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/header/header;";
+import styled from "styled-components";
+
+const Above = styled.div``;
+
+export default function TestResult() {
+  return (
+    <>
+      <Header />
+      <Above></Above>
+    </>
+  );
+}
