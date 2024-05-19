15dfb342b7c1b80ef5fc90145b34d02dc327629a
Seonghwan Sim <hoanshim01@handong.ac.kr>
2024-05-20 03:39:09 +0900

    style : 캐릭터 카드 추가

--- src/routes/TestResult.jsx
+++ src/routes/TestResult.jsx
@@ -1,13 +1,57 @@
-import Header from "../components/header/header;";
+import Header from "../components/header/header";
 import styled from "styled-components";
 
-const Above = styled.div``;
+import SlibFairyBig from "../imgs/silbifairy_big.svg";
+import MaepguSmall from "../imgs/maepgu_small.svg";
+import DownArrow from "../imgs/downArrow.svg";
+import UpArrow from "../imgs/upArrow.svg";
+
+const StyleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Above = styled.div`
+  display: flex;
+`;
+
+const AboveLeft = styled.div``;
+
+const AboveRight = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const Bottom = styled.div``;
 
 export default function TestResult() {
   return (
     <>
       <Header />
-      <Above></Above>
+      <StyleContainer>
+        <Above>
+          <AboveLeft>
+            <div>5단계 : 실비요정</div>
+            <img src={SlibFairyBig} alt="big" />
+          </AboveLeft>
+          <AboveRight>
+            <div>캐릭터 설명</div>
+            <button>지도 보러가기</button>
+          </AboveRight>
+        </Above>
+        <div>다른 캐릭터들 보러 가기</div>
+        <img src={DownArrow} alt="downarrow" />
+        <img src={UpArrow} alt="uparrow" />
+        <div>내 캐릭터 보러 가기</div>
+        <Bottom>
+          <img src={MaepguSmall} alt="1" />
+          <img src={MaepguSmall} alt="2" />
+          <img src={MaepguSmall} alt="3" />
+          <img src={MaepguSmall} alt="4" />
+        </Bottom>
+      </StyleContainer>
     </>
   );
 }
