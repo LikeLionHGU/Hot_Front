347f4cc92c212b4223dccf3611179c3b4cfb844e
Seonghwan Sim <hoanshim01@handong.ac.kr>
2024-06-07 02:04:02 +0900

    style : 로그인 아닐 때 리뷰 작성

---
+++ src/components/review/nonLogin.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+import CharacterLogo from "../../imgs/nonLoginLogo.svg";
+import Font from "../../assets/font.css";
+
+const Sidebar = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: row-reverse;
+  width: 100%;
+`;
+
+const SidebarContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Character = styled.img`
+  margin-top: 50px;
+`;
+
+const LoginBtn = styled.button`
+  border: 0px;
+  border-radius: 4px;
+
+  margin-top: 50px;
+  padding: 15px 60px;
+
+  background-color: #f6b95b;
+  color: #410a0a;
+  font-family: Dream5;
+`;
+
+export default function SideBar() {
+  return (
+    <Sidebar>
+      <SidebarContainer>
+        <Character src={CharacterLogo} alt="캐릭터 로고" />
+        <LoginBtn>로그인</LoginBtn>
+      </SidebarContainer>
+    </Sidebar>
+  );
+}
